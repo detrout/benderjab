40dba3ef5107220375cba87346f4880350a9530d
diane@ghic.org <>
2007-06-17 09:01:29 +0000

    [project @ add home example] since it involves a dice roller which I'd customized some, I might as well check in both parts of my example.

---
+++ home.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import re
+import sys
+
+from benderjab import BenderJab
+
+# this came from the pyparsing wiki http://pyparsing.wikispaces.com/Examples
+# slightly modified
+import dice2
+
+def parser(s, who=None):
+  try:
+    if re.match("roll", s):
+      return dice2.dice(s[4:])
+  except Exception, e:
+    return "failed:"+str(e)
+
+def loop_reporter():
+  print "."
+
+def main():
+  bot = BenderJab('bender@ghic.org')
+  bot.parser = parser
+  #bot.eventTasks.append(loop_reporter)
+  bot.logon()
+  bot.eventLoop()
+  return 0
+
+if __name__ == "__main__":
+  sys.exit(main())
