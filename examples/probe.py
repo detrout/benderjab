61dc81ec9b4d4a0886126c9f959e9920f110e72c
Diane Trout <diane@caltech.edu>
2012-12-03 00:11:01 -0800

    Add more commands for poking around ones xmpp clients.
    commands added:
     who - show user & resources visible to bot
     disco-items - jabber feature discovery
     disco-info  - jabber feature discovery
    
    Admittedly the UI for the discovery commands are poor.
    I just grab the xml xmpp payload, and use lxml to pretty print them
    to the bots stdout.

--- examples/probe.py
+++ examples/probe.py
@@ -3,10 +3,11 @@
 I was looking through the examples in XMPP: A Definitive Guide,
 and wanted an easier way to try some of their examples.
 """
-
+from lxml.etree import fromstring, dump
+import xmpp
 import shlex
 from benderjab import bot
-from benderjab.util import get_config
+from benderjab.util import get_config, toJID
 from benderjab.bargparse import BotArgumentParser
 
 from dice2 import dice
@@ -21,12 +22,29 @@ class ProbeBot(bot.BenderJab):
         helpparser = self.subparsers.add_parser('help')
         helpparser.set_defaults(func=self.format_help)
 
+    def logon(self, jid=None, password=None, resource=None):
+        super(ProbeBot, self).logon(jid, password, resource)
+        print "registering"
+        self.cl.RegisterHandler('iq', self.iqCB)
+
+
     def list_commands(self, arg):
         return u"Available commands: " + ", ".join(self.commands.keys())
 
+    def messageCB(self, conn, msg):
+        tree = fromstring(unicode(msg))
+        dump(tree)
+        return super(ProbeBot, self).messageCB(conn, msg)
+
     def presenceCB(self, conn, msg):
-        print "Presence:", str(msg)
+        tree = fromstring(unicode(msg))
+        dump(tree)
         return super(ProbeBot, self).presenceCB(conn, msg)
+
+    def iqCB(self, dis, stanza):
+        #print "dis:", unicode(dis)
+        tree = fromstring(unicode(stanza))
+        dump(tree)
 
     def format_help(self, args):
         return self.argparse.format_help()
@@ -36,16 +54,18 @@ class ProbeBot(bot.BenderJab):
             cmdline = shlex.split(message)
             args = self.argparse.parse_args(cmdline)
             print "args", args
+            args.frm = who
             if args.func:
                 return args.func(args)
             return u"Didn't understand:" + unicode(message)
 
-    def command(self, name):
+    def command(self, name, **kwargs):
         def wrapper(func):
             if not hasattr(func, SUBPARSER):
-                func.subparser = self.subparsers.add_parser(name)
+                func.subparser = self.subparsers.add_parser(name, **kwargs)
                 func.subparser.prog = name
                 func.subparser.set_defaults(func=func)
+                func.subparser.set_defaults(bot=self)
             return func
         return wrapper
 
@@ -59,7 +79,7 @@ class ProbeBot(bot.BenderJab):
 
 bot = ProbeBot()
 @bot.argument('strings', nargs='*', help='echo some text back')
-@bot.command('echo')
+@bot.command('echo', help="return your message")
 def echo(args):
     if args.strings:
         return u'You sent:'+u' '.join((unicode(s) for s in args.strings))
@@ -67,12 +87,83 @@ def echo(args):
 
 @bot.argument('dice', nargs='*', type=str,
               help='Specify a die formula. e.g. 2d4 + 2')
-@bot.command('roll')
+@bot.command('roll', help="Roll D&D-esque dice")
 def roll(args):
     if args.dice:
         return dice(' '.join(args.dice))
     parser = getattr(roll, SUBPARSER)
     return parser.format_usage()
+
+@bot.argument('-w', '--who', help="specify JID to query")
+@bot.argument('--node', nargs=1, help="specify a node attribute")
+@bot.command('disco-items', help="send a disco#items query")
+def disco_info(args):
+    if not args.who:
+        return self.subparser.format_usage()
+    who = toJID(args.who)
+    q = xmpp.Iq(typ="get",
+                queryNS='http://jabber.org/protocol/disco#items',
+                to=who)
+    if args.node:
+        q.setTagAttr('query', 'node', args.node[0])
+    args.bot.cl.send(q)
+    return u"Sent items query"
+
+@bot.argument('-w', '--who', help="specify JID to query")
+@bot.argument('--node', nargs=1, help="specify a node attribute")
+@bot.command('disco-info', help="send a disco#info query")
+def disco_info(args):
+    if not args.who:
+        return self.subparser.format_usage()
+    who = toJID(args.who)
+
+    q = xmpp.Iq(typ="get",
+                queryNS='http://jabber.org/protocol/disco#info',
+                to=who)
+    if args.node:
+        q.setTagAttr('query', 'node', args.node[0])
+    tree = fromstring(str(q))
+    print "--disco-info--"
+    dump(tree)
+    args.bot.cl.send(q)
+    return u"Sent info query"
+
+@bot.command('form', help="send me a form")
+def form(args):
+    form = '''<captcha xmlns="urn:xmpp:captcha">
+        <x xmlns="jabber:x:data" type="form">
+        <title>A form!</title>
+        <instructions>Fill in the form</instructions>
+        <field label="Text input" type="text_single" var="field-1"/>
+        <field label="Pick one" type="list-single" var="field-2">
+          <option label="First"><value>opt-1</value></option>
+          <option label="Second"><value>opt-2</value></option>
+        </field>
+        </x>
+        </captcha>'''
+    f = xmpp.Message(
+        to=args.frm,
+        payload=[xmpp.simplexml.XML2Node('<body>Form attached</body>'),
+                 xmpp.simplexml.XML2Node(form)])
+    tree = fromstring(str(f))
+    print "--form--"
+    dump(tree)
+    args.bot.cl.send(f)
+    return u"I hope you like it"
+
+@bot.command('who', help="list who is online")
+def who(args):
+    roster = args.bot.cl.getRoster()
+    reply = []
+    for jid in roster.keys():
+        resources = roster.getResources(jid)
+        if resources:
+            reply.append(unicode(jid))
+            for resource in resources:
+                reply.append(unicode(u'\t'+jid+'/'+resource))
+
+    return u"\n".join(reply)
+
 
 if __name__ == "__main__":
     bot.main()
